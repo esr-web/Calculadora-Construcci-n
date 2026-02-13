16732e21ac48e92f77999ed011825ce25cc0cf61
esr-web <emanuelsilvariambaut@gmail.com>
2026-02-13 06:56:54 -0600

    Update core.js

--- js/core.js
+++ js/core.js
@@ -12,3 +12,29 @@ function aplicarDesperdicio(cantidad, porcentaje) {
 function convertirMoneda(valorUSD, moneda) {
   return valorUSD * MONEDAS[moneda];
 }
+let PRESUPUESTO = {
+  cemento: 0,
+  arena: 0,
+  grava: 0,
+  acero: 0,
+  totalUSD: 0
+};
+
+function agregarPresupuesto(cemento, arena, grava, acero, costoUSD) {
+  PRESUPUESTO.cemento += cemento;
+  PRESUPUESTO.arena += arena;
+  PRESUPUESTO.grava += grava;
+  PRESUPUESTO.acero += acero;
+  PRESUPUESTO.totalUSD += costoUSD;
+}
+
+function limpiarPresupuesto() {
+  PRESUPUESTO = {
+    cemento: 0,
+    arena: 0,
+    grava: 0,
+    acero: 0,
+    totalUSD: 0
+  };
+  mostrarPresupuesto();
+}

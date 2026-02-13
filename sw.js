fc169aef954638eb9d32144a0ae4b9a7610bf9b4
esr-web <emanuelsilvariambaut@gmail.com>
2026-02-13 06:47:11 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-1
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("construccion-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./css/style.css",
+        "./js/precios.js",
+        "./js/core.js",
+        "./js/muros.js"
+      ]);
+    })
+  );
+});

df17947683660e66d31469d6e39ac37280fc18ad
esr-web <emanuelsilvariambaut@gmail.com>
2026-02-13 06:49:46 -0600

    Update core.js

--- js/core.js
+++ js/core.js
@@ -5,3 +5,10 @@ function redondear(valor, decimales = 2) {
 function costo(cantidad, precio) {
   return redondear(cantidad * precio);
 }
+function aplicarDesperdicio(cantidad, porcentaje) {
+  return cantidad * (1 + porcentaje / 100);
+}
+
+function convertirMoneda(valorUSD, moneda) {
+  return valorUSD * MONEDAS[moneda];
+}

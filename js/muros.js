806b9a62dba9c71e0375e91808bcfd65875e56c2
esr-web <emanuelsilvariambaut@gmail.com>
2026-02-14 17:39:34 -0600

    Update muros.js

--- js/muros.js
+++ js/muros.js
@@ -85,11 +85,7 @@ function calcularMuro() {
 }
 document.getElementById("moneda").addEventListener("change", function () {
   const moneda = this.value;
-  document.getElementById("tituloPrecios").innerHTML = `
-    Precios (${moneda})
-    <select id="moneda">
-      <option value="CUP" ${moneda === "CUP" ? "selected" : ""}>CUP</option>
-      <option value="USD" ${moneda === "USD" ? "selected" : ""}>USD</option>
+  
     </select>
   `;
 });
